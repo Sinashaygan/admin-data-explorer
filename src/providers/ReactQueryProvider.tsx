00dd8ef6ec664a80eeb251a8ae3309db63f57a8a
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-14 19:49:52 +0330

    feat:add the react query provider

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 5 * 1000,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
