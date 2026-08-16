426ba7487e192331c1640b3d828b3675e0feb16a
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 15:56:00 +0330

    feat: give the query of db for hidration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import { Stack, Typography } from "@mui/material";
 import { OrdersGrid } from "../features/orders/components/OrdersGrid";
 import { OrdersFilters } from "../features/orders/components/OrdersFilters";
 import { QueryClient } from "@tanstack/react-query";
+import { orderFiltersFromSearchParams } from "../features/orders/model/order-url.mapper";
+import { toOrdersDatabaseQuery } from "../features/orders/model/order.query-mapper";
 
 export const metadata = {
   title: "Orders Management | Admin Dashboard",
@@ -13,8 +15,24 @@ type OrderPagePops = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
-export default function OrderPage({ searchParams }: OrderPagePops) {
+export default async function OrderPage({ searchParams }: OrderPagePops) {
   const queryClient = new QueryClient();
+  const resolvedParams = await searchParams;
+
+   const paramsAdapter = {
+     get: (name: string) => {
+       const val = resolvedParams[name];
+       return Array.isArray(val) ? val[0] : (val ?? null);
+     },
+     getAll: (name: string) => {
+       const val = resolvedParams[name];
+       if (!val) return [];
+       return Array.isArray(val) ? val : [val];
+     },
+   };
+
+   const filters = orderFiltersFromSearchParams(paramsAdapter);
+   const dbQuery = toOrdersDatabaseQuery(filters);
 
   return (
     <Stack spacing={2}>
