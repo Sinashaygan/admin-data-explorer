5ab832b1c7c4951bc2471927c92b635fec744745
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 10:05:16 +0330

    feat: set the time out with the delay

--- src/features/orders/hooks/useDebouncedValue.ts
+++ src/features/orders/hooks/useDebouncedValue.ts
@@ -1,5 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useDebouncedValue<T>(value:T , delay=400){
     const [debouncedValue , setDebounceValue] = useState(value)
+
+    useEffect(()=>{
+        const timeoutId = window.setTimeout(()=>{
+            setDebounceValue(value)
+        }, delay)
+    },[value , delay])
+
+    return debouncedValue
 }
