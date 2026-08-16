4320d7466cca17d0a73b8b49e791627457260a33
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-17 02:13:49 +0330

    feat: add the selector on orderstatusMenu

---
+++ src/features/orders/components/OrderStatusMenu.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  CircularProgress,
+  FormControl,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
+
+import { useUpdateOrderStatus } from "../hooks/useUpdateOrderStatus";
+import type { OrderStatus } from "../model/order.types";
+import { ORDER_STATUSES } from "../model/order.constants";
+
+type OrderStatusMenuProps = {
+  orderId: string;
+  value: OrderStatus;
+};
+
+export function OrderStatusMenu({ orderId, value }: OrderStatusMenuProps) {
+  const mutation = useUpdateOrderStatus();
+
+  const handleChange = (event: SelectChangeEvent<OrderStatus>) => {
+    mutation.mutate({
+      orderId,
+      status: event.target.value as OrderStatus,
+    });
+  };
+
+  return (
+    <FormControl size="small" sx={{ minWidth: 130 }}>
+      <Select
+        value={value}
+        onChange={handleChange}
+        disabled={mutation.isPending}
+        inputProps={{
+          "aria-label": "Change order status",
+        }}
+      >
+        {ORDER_STATUSES.map((status) => (
+          <MenuItem key={status} value={status}>
+            {status}
+          </MenuItem>
+        ))}
+      </Select>
+
+      {mutation.isPending && (
+        <CircularProgress
+          size={16}
+          sx={{
+            position: "absolute",
+            right: 28,
+            top: 12,
+          }}
+        />
+      )}
+    </FormControl>
+  );
+}
