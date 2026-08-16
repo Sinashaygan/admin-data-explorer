733e6c65c33d1e707c0ecd546947a8114797e683
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-17 01:32:34 +0330

    feat: updateOrderStatus with orderId

--- src/app/api/orders/[orderId]/status/route.ts
+++ src/app/api/orders/[orderId]/status/route.ts
@@ -1,3 +1,5 @@
+import { updateOrderStatus } from "@/src/features/orders/api/orders.repository";
+import { NextResponse } from "next/server";
 import z from "zod";
 
 const updateStatusSchemas = z.object({
@@ -16,4 +18,39 @@ type RouteContext = {
   }>;
 };
 
-export async function PATCH(request: Request, context: RouteContext) {}
+export async function PATCH(request: Request, context: RouteContext) {
+     try {
+       const { orderId } = await context.params;
+
+       if (!orderId) {
+         return NextResponse.json(
+           { message: "Order id is required" },
+           { status: 400 },
+         );
+       }
+
+       const body: unknown = await request.json();
+       const parsedBody = updateStatusSchemas.safeParse(body);
+
+       if (!parsedBody.success) {
+         return NextResponse.json(
+           {
+             message: "Invalid order status",
+             issues: parsedBody.error.flatten(),
+           },
+           { status: 422 },
+         );
+       }
+
+       const order = await updateOrderStatus(orderId, parsedBody.data.status);
+
+       return NextResponse.json(order);
+     } catch (error) {
+       const message =
+         error instanceof Error
+           ? error.message
+           : "Failed to update order status";
+
+       return NextResponse.json({ message }, { status: 500 });
+     }
+}
