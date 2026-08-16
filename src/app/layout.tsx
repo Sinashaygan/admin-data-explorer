bbce885991d298681d7f606ca97505e37ceb8e7d
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 12:29:04 +0330

    fix: configure mui app router cache provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import ReactQueryProvider from "../providers/ReactQueryProvider";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter"; 
 
 export default function RootLayout({
   children,
@@ -8,7 +9,9 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        <AppRouterCacheProvider>
+          <ReactQueryProvider>{children}</ReactQueryProvider>
+        </AppRouterCacheProvider>
       </body>
     </html>
   );
