4972f44595b911661740a0c470a0bff35a9e22dc
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 10:30:05 +0330

    feat: add debounceSearch

--- src/features/orders/components/OrdersFilters.tsx
+++ src/features/orders/components/OrdersFilters.tsx
@@ -1,5 +1,11 @@
+import { useState } from "react";
 import { useOrderFilters } from "../hooks/useOrderFilters";
+import { useDebouncedValue } from "../hooks/useDebouncedValue";
 
 export function OrdersFilters() {
     const {filters , resetFilters , setFilters} = useOrderFilters()
+
+    const [searchInput , setSearchInput] = useState(filters.search)
+
+    const debouncedSearch = useDebouncedValue(searchInput , 450)
 }
