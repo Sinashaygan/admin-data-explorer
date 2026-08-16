51b48bbe556e7f9fb99798bd04e0a7892c34bab3
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 15:50:26 +0330

    feat: add the type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,25 @@ import { Stack, Typography } from "@mui/material";
 
 import { OrdersGrid } from "../features/orders/components/OrdersGrid";
 import { OrdersFilters } from "../features/orders/components/OrdersFilters";
+import { QueryClient } from "@tanstack/react-query";
 
 export const metadata = {
   title: "Orders Management | Admin Dashboard",
   description: "View and manage customer orders efficiently.",
 };
 
-export default function Home() {
+type OrderPagePops = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+
+export default function OrderPage({ searchParams }: OrderPagePops) {
+  const queryClient = new QueryClient();
+
   return (
     <Stack spacing={2}>
       <Typography variant="h5">Orders</Typography>
 
-      <OrdersFilters/>
+      <OrdersFilters />
 
       <OrdersGrid />
     </Stack>
