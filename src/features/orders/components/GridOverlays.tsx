87dff8c040de8be965c4f2cefdfce098a8118de0
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 13:27:04 +0330

    feat: add a component for not rows overally

--- src/features/orders/components/GridOverlays.tsx
+++ src/features/orders/components/GridOverlays.tsx
@@ -1,6 +1,27 @@
 import { Box, CircularProgress, Typography } from "@mui/material";
 
 export function CustomLoadingOverlay() {
+  <Box
+    sx={{
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%",
+      gap: 2,
+      backgroundColor: "rgba(255, 255, 255, 0.6)",
+      zIndex: 1,
+    }}
+  >
+    <CircularProgress size={40} thickness={4} />
+    <Typography variant="body2" color="textSecondary">
+      Loading Orders...
+    </Typography>
+  </Box>;
+}
+
+export function CustomNoRowsOverlay() {
+  return (
     <Box
       sx={{
         display: "flex",
@@ -8,14 +29,9 @@ export function CustomLoadingOverlay() {
         alignItems: "center",
         justifyContent: "center",
         height: "100%",
-        gap: 2,
-        backgroundColor: "rgba(255, 255, 255, 0.6)",
-        zIndex: 1,
+        gap: 1,
       }}
     >
-        <CircularProgress size={40} thickness={4}/>
-        <Typography variant="body2" color="textSecondary">
-            Loading Orders...
-        </Typography>
-    </Box>;
+    </Box>
+  );
 }
