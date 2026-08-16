b1a06bb60081f6e7763f6d558b1c66e13b29bb6b
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-16 15:48:17 +0330

    feat: add the metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,11 @@ import { Stack, Typography } from "@mui/material";
 import { OrdersGrid } from "../features/orders/components/OrdersGrid";
 import { OrdersFilters } from "../features/orders/components/OrdersFilters";
 
+export const metadata = {
+  title: "Orders Management | Admin Dashboard",
+  description: "View and manage customer orders efficiently.",
+};
+
 export default function Home() {
   return (
     <Stack spacing={2}>
