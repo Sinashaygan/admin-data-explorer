d6e8fa39741416a3f40589351cf822d4186ea593
Sinashaygan <sinashaygan23041385@gmail.com>
2026-08-17 10:02:27 +0330

    feat: add snackProvider

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,23 +1,31 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode } from "react";
+import { SnackbarProvider } from "notistack";
+import { ReactNode, useState } from "react";
 
 export default function ReactQueryProvider({
   children,
 }: {
   children: ReactNode;
 }) {
-  const queryClient = new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 5 * 1000,
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 5 * 1000,
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SnackbarProvider maxSnack={3} anchorOrigin={{ vertical: "bottom", horizontal: "right" }}>
+        {children}
+      </SnackbarProvider>
+    </QueryClientProvider>
   );
 }
